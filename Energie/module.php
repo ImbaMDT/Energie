6f3ca27a4422f8582329f4c1966add92a82260f3
ImbaMDT <45270806+ImbaMDT@users.noreply.github.com>
2025-11-14 15:58:37 +0100

    Update module.php

--- Energie/module.php
+++ Energie/module.php
@@ -13,6 +13,30 @@ class Energie extends IPSModule
     public function ApplyChanges()
     {
         parent::ApplyChanges();
+
+        // Verzeichnis vom Modul ermitteln
+        $moduleDir = IPS_GetKernelDir() . 'modules' . DIRECTORY_SEPARATOR . 'ImbaMDT.Energie' . DIRECTORY_SEPARATOR . 'scripts';
+    
+        // Nur wenn Verzeichnis existiert
+        if (is_dir($moduleDir)) {
+            $files = scandir($moduleDir);
+            foreach ($files as $file) {
+                if (pathinfo($file, PATHINFO_EXTENSION) === 'php') {
+                    $ident = pathinfo($file, PATHINFO_FILENAME);
+                    $existingID = @IPS_GetObjectIDByIdent($ident, $this->InstanceID);
+                    if ($existingID === false) {
+                        $scriptID = IPS_CreateScript(0);
+                        IPS_SetName($scriptID, $ident);
+                        IPS_SetIdent($scriptID, $ident);
+                        IPS_SetParent($scriptID, $this->InstanceID);
+    
+                        // Inhalt aus Datei laden und setzen
+                        $code = file_get_contents($moduleDir . DIRECTORY_SEPARATOR . $file);
+                        IPS_SetScriptContent($scriptID, $code);
+                    }
+                }
+            }
+        }
     }
 
     public function DeployScripts()
@@ -46,4 +70,5 @@ class Energie extends IPSModule
             $this->SendDebug('Energie', "Skript $scriptName deployed (ID: $id)", 0);
         }
     }
+
 }
